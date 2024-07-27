4fd4e690bb21fb2bbdd9ebed4532dce4ad13d89a
Jony <jonybhattacharjee886@gmail.com>
2024-07-28 00:48:56 +0600

    style: :art: Service Page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,9 @@
+import { ServiceComponent } from "./_components/ServiceComponent";
+
 export default function Services() {
-  return <div>Services</div>;
+  return <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <div className="container mx-auto">
+      <ServiceComponent />
+    </div>
+  </section>;
 }
